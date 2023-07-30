7cec3904d0673f1eb5c1fb884da64c96cba0ab37
Latisha Copeland <lcopeland001@gmail.com>
2023-07-30 09:25:05 -0500

    update blog router get routes

--- server/routes/blog.router.js
+++ server/routes/blog.router.js
@@ -5,8 +5,18 @@ const router = express.Router();
 
 // GET route
 router.get('/', (req, res) => {
-  const queryText = 'SELECT * FROM "blog" WHERE user_id = $1';
-  pool.query(queryText, [req.user.id]).then((result) => {
+  const queryText = 'SELECT id, name FROM blog';
+  pool.query(queryText).then((result) => {
+    res.send(result.rows);
+  }).catch((error) => {
+    console.log(error);
+    res.sendStatus(500);
+  });
+});
+
+router.get('/details/:id', (req, res) => {
+  const queryText = 'SELECT * FROM "blog" WHERE id=$1';
+  pool.query(queryText, [req.params.id]).then((result) => {
     res.send(result.rows);
   }).catch((error) => {
     console.log(error);
